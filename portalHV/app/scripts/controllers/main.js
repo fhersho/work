f087e2eb645f221320cab32d737742e86fb4441e
fhersho <fhersho@gmail.com>
2015-11-10 19:02:23 -0500

    creacion del proyecto

---
+++ portalHV/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portalHvApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the portalHvApp
+ */
+angular.module('portalHvApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
